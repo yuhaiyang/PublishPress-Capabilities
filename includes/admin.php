3f7f14d91f437612cd15538c7c0801ac4574bebc
Valentín García <htmgarcia@gmail.com>
2020-03-24 01:03:30 +0900

    Edit, delete, read checkboxes consistency in mobile

--- includes/admin.php
+++ includes/admin.php
@@ -357,7 +357,7 @@ if( defined('PRESSPERMIT_ACTIVE') ) {
                                                 $disabled = '';
                                                 $checked = checked(1, ! empty($rcaps[$cap_name]), false );
 
-                                                $checkbox = '<input type="checkbox"' . $title . ' name="caps[' . $cap_name . ']" value="1" ' . $checked . $disabled . ' />';
+                                                $checkbox = '<input type="checkbox"' . $title . ' name="caps[' . $cap_name . ']" value="1" ' . $checked . $disabled . ' /><span class="pp-checkbox-dynamic-label">' . ucwords($prop) . '</span>';
 
                                                 $type_caps [$cap_name] = true;
                                                 $display_row = true;
